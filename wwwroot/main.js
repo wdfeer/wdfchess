4659475e6ab2b2a80e830d8b32f8f600c71119a0
wdfeer <49950781+wdfeer@users.noreply.github.com>
2022-12-28 16:35:49 +0100

    Draw chessboard

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,5 +1,7 @@
 const connectForm = document.querySelector('#connect-form');
 const connectedSuccessfully = document.querySelector('#connected-text');
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 function connect() {
     const hostname = document.querySelector('#ip-input').value;
     const port = document.querySelector('#port-input').value;;
@@ -18,9 +20,26 @@ function connect() {
             `Successfully connected to <br>
             ${hostname}:${port}`;
         connectedSuccessfully.classList.remove('disabled');
+        setTimeout(() => {
+            connectedSuccessfully.classList.add('disabled');
+            canvas.classList.remove('disabled');
+            drawBoard();
+        }, 1000);
     };
 
     socket.onclose = () => {
         console.log('CLOSED');
     };
+}
+
+const squareSize = canvas.width / 8;
+function drawBoard() {
+    for (var i = 0; i < 8; i++) {
+        for (var j = 0; j < 8; j++) {
+            ctx.beginPath();
+            ctx.fillStyle = ["#eeeed2", "#630"][(i + j) % 2];
+            ctx.fillRect(j * squareSize, i * squareSize, squareSize, squareSize);
+            ctx.closePath();
+        }
+    }
 }
